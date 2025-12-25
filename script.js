7f0b8ca925b7229e4184a6883428d081f187adb4
Asus <waghshailesh151@gmail.com>
2025-12-25 15:28:44 +0530

    trying

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
 alert('Hello from the first page!');
 console.log('First day learning JavaScript');
 console.log('I am shailesh wagh');
+product = 'laptop';
+price = 45000;
+console.log('Product:', product);
+console.log('Price:', price);
